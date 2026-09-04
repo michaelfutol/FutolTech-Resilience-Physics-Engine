a12ccbe4d0f5eae463742a2ed1eba561df16371f
Michael Futol <michaelxfutol@gmail.com>
2026-09-04 07:48:10 -0700

    Wire quantity overrides into prototype editor UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,11 +39,13 @@ export default function Home() {
           draftHasChanges={modelData.draftHasChanges}
           draftHasCostOverrides={modelData.draftHasCostOverrides}
           costRateOverrides={modelData.costRateOverrides}
+          quantityOverrides={modelData.quantityOverrides}
           baselineCost={modelData.baselineCost}
           draftCost={modelData.draftCost}
           updateDraftAssembly={modelData.updateDraftAssembly}
           updateCostRateOverride={modelData.updateCostRateOverride}
-          clearCostRateOverrides={modelData.clearCostContextOverrides}
+          updateQuantityOverride={modelData.updateQuantityOverride}
+          clearCostContextOverrides={modelData.clearCostContextOverrides}
           resetDraft={modelData.resetDraft}
           createCandidate={modelData.createCandidate}
           createdCandidate={modelData.createdCandidate}
